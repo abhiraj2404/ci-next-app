5952c635082ec9e6dcb658df1c1281ea027640d3
Abhiraj Singh Chauhan <chauhanabhiraj06@gmail.com>
2025-10-07 18:29:52 +0530

    feat: http and ws server inititalize

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello from http server");
+import express from "express";
+import { client } from "@repo/db/client";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from http server");
+});
+
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const user = await client.user.create({
+    data: {
+      username: username,
+      password: password
+    }
+  })
+
+  res.json({
+    status: "user created",
+    data: { user },
+  });
+});
+
+app.listen(3001, () => {
+  console.log("HTTP server running on port 3001");
+});
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello from ws server");
+import { WebSocketServer } from "ws";
+import { client } from "@repo/db/client";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", async function connection(ws) {
+  const username = Math.random().toString();
+  const password = Math.random().toString();
+  const user = await client.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+
+  ws.send("user created successfully");
+});
